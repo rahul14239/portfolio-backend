18fcc31564e78f1c44c1143c4a2d04a22f1a576a
rahul14329 <rn0553691@gmail.com>
2026-03-25 16:11:27 +0530

    added test endpoint

--- src/main/java/portfolio/com/controller/ContactController.java
+++ src/main/java/portfolio/com/controller/ContactController.java
@@ -1,10 +1,7 @@
 package portfolio.com.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import portfolio.com.entity.Contact;
 import portfolio.com.repository.ContactRepository;
 
@@ -16,6 +13,11 @@ public class ContactController {
 
     ContactController(ContactRepository repository){
         this.repository=repository;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "App is running!";
     }
 
     @PostMapping("/contact")
